fba032d4ae6eff99035dde47fc07f7b8ed8b670e
Zander Allen <ztrain01@gmail.com>
2015-02-24 22:16:34 -0700

    cleaned up code

--- FriendController.js
+++ FriendController.js
@@ -1,3 +1,16 @@
+var app = angular.module('AngularApp')
+
+app.controller
+
+
+
+var updateFriendCount = function(arr) {
+  for (var = 0; i < arr.length; i++) {
+    console.log(arr[i].friend_count);
+    $scope.friend[i].friend_count = parseInt.friend_count;
+  }
+
+}
 
 
 
